761ce0f5712c60e4c14340891103685338851464
Angela <patataramirez13@gmail.com>
2020-04-21 13:38:15 -0500

    Added the handlebars pages

--- server.js
+++ server.js
@@ -1,8 +1,23 @@
 const express = require('express');
 
+const path = require('path');
+
+const exphbs = require('express-handlebars');
+
 const app = express();
 
+app.engine('handlebars', exphbs());
+app.set('view engine', 'handlebars');
+
 app.use(express.static('public'));
+
+app.get('/home', function (request, response) {
+  response.sendFile(path.join(__dirname, '/public/index.html'));
+});
+
+app.get('/store', function (req, res) {
+  res.render('shop');
+});
 
 app.listen(3000, function () {
     console.log('servidor iniciado');

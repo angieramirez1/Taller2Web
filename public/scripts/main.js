c32df3672d7570c69f426665566350f7e7b30ded
Angela <patataramirez13@gmail.com>
2020-04-21 12:39:39 -0500

    Added the main page

---
+++ public/scripts/main.js
@@ -0,0 +1,10 @@
+
+function red() {
+        document.getElementById('product').src= "./images/red switch.png";
+    }
+function blue() {
+    document.getElementById('product').src= "./images/blue switch.png";
+    }
+function orange() {
+    document.getElementById('product').src= "./images/yellow switch.png";
+    }

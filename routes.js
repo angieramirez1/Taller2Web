768b2cc2e8cf81ca7f2b73efe157c2997905be1b
angieramirez1 <patataramirez13@gmail.com>
2020-05-10 15:36:37 -0500

    Added database with mongo

---
+++ routes.js
@@ -0,0 +1,68 @@
+const assert = require('assert');
+const ObjectId = require('mongodb').ObjectId;
+
+function configureRoutes (app,db) {
+      
+      app.get('/store', function (req, res) {
+        
+        var filters = {}
+
+        if(req.query.price_lt){
+            filters.price = {
+                $lt: parseInt(req.query.price_lt)
+            }
+        }
+        if(req.query.price_gt){
+            filters.price = {
+                $gt: parseInt(req.query.price_gt)
+            }
+        }
+        if(req.query.rating_gt){
+            filters.rating = {
+                $gt: parseInt(req.query.rating_gt)
+            }
+        }
+        if(req.query.rating_lt){
+            filters.rating = {
+                $lt: parseInt(req.query.rating_lt)
+            }
+        }
+        
+        if(req.query.search){
+            filters.playerMode = {
+                    $regex: new RegExp(req.query.search, 'i')
+                }
+        }
+
+        const collection = db.collection('products');
+        // Find some documents
+        collection.find(filters).toArray(function(err, docs) {
+          assert.equal(err, null);
+          var context = {
+            list: docs,
+          }
+          res.render('shop', context);
+        });
+
+
+        
+      });
+      
+      app.get('/product/:name/:id', function (req, res) {
+        const filter = {
+            _id: {
+                $eq: new ObjectId(req.params.id)
+            }
+        };
+
+        const collection = db.collection('products');
+        // Find some documents
+        collection.find(filter).toArray(function(err, docs) {
+          assert.equal(err, null);
+          var context = docs[0]
+          res.render('product', context);
+        });
+      });
+}
+
+module.exports = configureRoutes;
--- server.js
+++ server.js
@@ -1,10 +1,14 @@
+const MongoClient = require('mongodb').MongoClient;
+
+const assert = require('assert');
+
 const express = require('express');
 
 const path = require('path');
 
 const exphbs = require('express-handlebars');
 
-const products = require('./products')
+const configureRoutes = require('./routes')
 
 const app = express();
 
@@ -13,53 +17,27 @@ app.set('view engine', 'handlebars');
 
 app.use(express.static('public'));
 
+// Connection URL
+const url = 'mongodb://localhost:27017';
+
+// Database Name
+const dbName = 'store_switch';
+
+// Create a new MongoClient
+const client = new MongoClient(url);
+
+// Use connect method to connect to the Server
+client.connect(function(err) {
+  assert.equal(null, err);
+  console.log("Connected successfully to server");
+
+  const db = client.db(dbName);
+
+  configureRoutes(app,db);
+});
 app.get('/home', function (request, response) {
   response.sendFile(path.join(__dirname, '/public/index.html'));
 });
-
-app.get('/store', function (req, res) {
-  
-  var filtered = products;
-  
-  if(req.query.price_lt){
-    filtered = products.filter(function(elem){
-      if(elem.price < req.query.price_lt){
-        return true;
-      }
-    });
-  }
-  if(req.query.price_gt){
-    filtered = products.filter(function(elem){
-      if(elem.price > req.query.price_gt){
-        return true;
-      }
-    });
-  }
-  
-  if(req.query.search){
-    filtered = products.filter(function(elem){
-      if(elem.playerMode.includes(req.query.search)){
-        return true;
-      }
-    });
-  }
-  var context = {
-    list: filtered,
-  }
-  res.render('shop', context);
-});
-
-app.get('/product/:name/:id', function (req, res) {
-  var context = {};
-  var foundElement =  products.find(function(elem){
-    if(elem.title == req.params.name){
-      return true;
-    }
-  });
-  context = foundElement;
-  res.render('product', context);
-});
-
 app.listen(3000, function () {
     console.log('servidor iniciado');
   });
